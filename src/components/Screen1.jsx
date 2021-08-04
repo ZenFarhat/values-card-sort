b71cbfa15fecd35d47640108f454d0bbf39b885d
Zen F <zeinfarhat6@gmail.com>
2021-08-04 14:44:44 +1000

    screen 1 done

--- src/components/Screen1.jsx
+++ src/components/Screen1.jsx
@@ -5,7 +5,11 @@ import card0 from '../assets/1/0.jpg'
 function Screens() {
   return (
     <div className='screen__1'>
-      <h1 className='screen__header'>Instructions</h1>
+      <div className='screen__header-container'>
+        <button className='prev__button page__btn'>Back</button>
+        <h1 className='screen__header'>Instructions</h1>
+        <button className='next__button page__btn'>Next</button>
+      </div>
       <h2 className='screen__subheader'>
         We are now going to consider what your own values are.
       </h2>
@@ -14,6 +18,20 @@ function Screens() {
         click to drag it.
       </h2>
       <img src={card0} alt='' className='card' />
+      <div className='drag__container'>
+        <div className='drag__card'>
+          <h1 className='card__header'>Very important to me</h1>
+          <div className='card__drag-area'></div>
+        </div>
+        <div className='drag__card'>
+          <h1 className='card__header'>Important to me</h1>
+          <div className='card__drag-area'></div>
+        </div>
+        <div className='drag__card'>
+          <h1 className='card__header'>Not important to me</h1>
+          <div className='card__drag-area'></div>
+        </div>
+      </div>
     </div>
   )
 }
