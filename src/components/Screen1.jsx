e5dd63d252c74041a355cdb1f0f3f276ff085de7
Zen F <zeinfarhat6@gmail.com>
2021-08-04 14:23:00 +1000

    screen 1 styling

--- src/components/Screen1.jsx
+++ src/components/Screen1.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import '../styles/style.css'
+import card0 from '../assets/1/0.jpg'
 
-function Screen1() {
+function Screens() {
   return (
     <div className='screen__1'>
-      <h1>SCREEN 1</h1>
+      <h1 className='screen__header'>Instructions</h1>
+      <h2 className='screen__subheader'>
+        We are now going to consider what your own values are.
+      </h2>
+      <h2 className='screen__subheader'>
+        You will be shown a card like the one below. Hover over the card and
+        click to drag it.
+      </h2>
+      <img src={card0} alt='' className='card' />
     </div>
   )
 }
 
-export default Screen1
+export default Screens
